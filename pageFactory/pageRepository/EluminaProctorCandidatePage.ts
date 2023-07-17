feb8a79ad42335f5d2e4dfd1399c03d75f5ef615
unknown <divyashree.r@igsindia.net>
2023-07-17 19:52:13 +0530

    Added more regression testcases and updated changes in smoke suite

--- pageFactory/pageRepository/EluminaProctorCandidatePage.ts
+++ pageFactory/pageRepository/EluminaProctorCandidatePage.ts
@@ -66,6 +66,11 @@ export class EluminaProctorCandidatePage {
     readonly SIGNOUT_BUTTON: Locator;
     readonly USERNAME_EDITBOX: Locator;
     readonly PASSWORD_EDITBOX: Locator;
+    readonly inceaseSize:Locator;
+    readonly iProctorExtension:Locator;
+    readonly cameraEnabled:Locator;
+    readonly microphoneEnabled:Locator;
+    readonly screenCaptureEnabled:Locator;
  
 
     constructor(page: Page, context: BrowserContext/*page1:Page,context1: BrowserContext*/) {
@@ -102,6 +107,11 @@ export class EluminaProctorCandidatePage {
         this.flagForReviewQuestions=page.locator('//div[text()="Flag for Review"]');
         this.clickOnPreviousBtn=page.locator('//div[@class="btn parent-body-container btn-primary"][normalize-space()="Previous"]');
         this.timeRemainingclock=page.locator('//div[@class="clock-text"]');
+        this.inceaseSize=page.locator('//em[@title="Increase Font Size"]');
+        this.iProctorExtension=page.locator('//div[@class="list-item"]//p[text()="iProctor Extension"]');
+        this.cameraEnabled=page.locator('//div[@class="list-item"]//p[text()="Camera"]');
+        this.microphoneEnabled=page.locator('//div[@class="list-item"]//p[text()="Microphone"]');
+        this.screenCaptureEnabled=page.locator('//div[@class="list-item"]//p[text()="Screen Capture"]');
     }
 
     /**Method to Navigate to Candidate URL */
@@ -134,6 +144,23 @@ export class EluminaProctorCandidatePage {
         await this.page.waitForTimeout(5000);
         await this.SIGNOUT_BUTTON.click();
 
+    }
+    async startExam(){
+        await this.ClickStartExamLink.click();
+        await this.page.waitForTimeout(2000);
+        await this.ClickOnUnderstand.click();
+        await this.page.waitForTimeout(2000);
+
+        console.log(await this.iProctorExtension.textContent());
+        await this.iProctorExtension.isVisible();
+        console.log(await this.cameraEnabled.textContent());
+        await this.cameraEnabled.isVisible();
+        console.log(await this.microphoneEnabled.textContent());
+        await this.microphoneEnabled.isVisible();
+        console.log(await this.screenCaptureEnabled.textContent());
+        await this.screenCaptureEnabled.isVisible();
+        await this.page.waitForTimeout(3000);
+        await this.page.screenshot({ path: 'screenshot.png', fullPage: true });
     }
 
     /**Method to do Invalid Login */
@@ -196,11 +223,6 @@ export class EluminaProctorCandidatePage {
        await this.ClickOnStartExamBtn.click();
        await this.page.waitForTimeout(3000);
     }
-        /**Method to Click On Start Exam Button*/
-    async clickOnStartExamBtn(){
-        await this.ClickOnStartExamBtn.click();
-       await this.page.waitForTimeout(3000);
-    }
 
     /**Method to start Candidate Exam and click on Next without answering questions*/
     async candidateStartExams(): Promise<void>{
@@ -233,12 +255,6 @@ export class EluminaProctorCandidatePage {
             await this.ClickOnNextBtn.click();
             await this.page.locator('//div[@class="question-number-container"]//div//p').last().click();
             await this.ClickOnRevieweBtn.click();
-    }
-
-      /**Method to Click on single  MCQ Quetions */
-      async candidateCliclkOnSingleMCQ(){
-        await this.page.locator('//div[@class="question-number-container"]//div//p').first().click()
-        await this.page.waitForTimeout(3000);
     }
 
     /**Method to Flag for Review the MCQ Quetions */
@@ -328,6 +344,14 @@ export class EluminaProctorCandidatePage {
         await this.page.waitForTimeout(2000);
         await this.ClickOnNextBtnPrct.click();
         await this.page.waitForTimeout(10000);
+    }
+
+    /**Method to Increase Font Size */
+    async increaseFontSize(){
+        await this.inceaseSize.click();
+        await this.inceaseSize.click();
+        await this.inceaseSize.click();
+        await this.page.waitForTimeout(5000);
     }
 
 }
---
+++ tests/regression/EluminaRegressioniProctorTC_021.test.ts
@@ -0,0 +1,71 @@
+import test from '@lib/Fixtures';
+import { chromium } from '@playwright/test';
+
+
+const devTestData = JSON.parse(JSON.stringify(require('../../enviroment-variables/dev/testData.json')));
+const p7TestData = JSON.parse(JSON.stringify(require('../../enviroment-variables/p7/testData.json')));
+const productionTestData = JSON.parse(JSON.stringify(require('../../enviroment-variables/production/testData.json')));
+const qaTestData = JSON.parse(JSON.stringify(require('../../enviroment-variables/qa/testData.json')));
+const sandboxTestData = JSON.parse(JSON.stringify(require('../../enviroment-variables/sandbox/testData.json')));
+const stagingTestData = JSON.parse(JSON.stringify(require('../../enviroment-variables/staging/testData.json')));
+
+let testData = qaTestData;
+if (process.env.ENV == 'dev') {
+    testData = devTestData;
+}
+else if(process.env.ENV == 'p7'){
+    testData = p7TestData;
+} 
+else if(process.env.ENV == 'production'){
+    testData = productionTestData;
+} 
+else if(process.env.ENV == 'qa'){
+    testData = qaTestData;
+} 
+else if(process.env.ENV == 'sandbox'){
+    testData = sandboxTestData;
+} 
+else if(process.env.ENV == 'staging'){
+    testData = stagingTestData;
+} 
+
+/**Validation of Changing Font Size to increase on the Dashboard */
+
+test(`@Regression Validation of Browser Reload option on Candidate Dashboard`, async ({ eluminaProctorCand,webActions }) => {
+    await test.step(`Navigate to Application`, async () => {
+        eluminaProctorCand.candidateNavigateToURL();
+        });
+        await test.step(`Candidate Login to application`, async () => {
+            await eluminaProctorCand.candidateLoginToApplications();
+        });
+
+        await test.step('Candidate start the exam',async ()=> {
+            await eluminaProctorCand.clickOnAllLink();
+             const browser = await chromium.launch();
+             const context1 = await browser.newContext();
+             const page1 = await context1.newPage();
+             await page1.goto('/');
+             await page1.waitForLoadState();
+             await page1.locator('(//input)[1]').type(testData.invigilatorUsername);
+             await page1.locator('(//input)[2]').type(testData.invigilatorPassword);
+             await page1.locator('//*[@class="submit-butn"]').click();
+             const [newPage] = await Promise.all([
+                 context1.waitForEvent('page'),
+                 await page1.locator('//div[text()="iAuthor"]').click()
+               ]);
+             
+             await newPage.locator('(//table[@class="table"]//tbody//tr[1]//td[2]//span)[1]').click();
+             await newPage.locator('//span[@class="thtext"]//input[@type="checkbox"]').click();
+             await newPage.locator('//div[@class="dropdown closed more-btn--width"]').click();
+             await newPage.locator('//p[text()="Verify Identity"]').click();
+             await newPage.locator('(//button[text()="Yes"])[1]').click();
+             await newPage.waitForTimeout(5000);
+             await newPage.close();
+             await page1.close();
+     
+         });
+         await test.step('Candidate start the exam',async ()=> {
+            await eluminaProctorCand.enterInvigilatorPassword();
+            await eluminaProctorCand.increaseFontSize();
+        });
+});
